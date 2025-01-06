903a3003606fb54db5ed9edbd63b26cf77b6911f
MaximeOriot <maxime9278@gmail.com>
2025-01-06 14:18:27 +0100

    queen

--- src/core/entities/pawn.model.tsx
+++ src/core/entities/pawn.model.tsx
@@ -2,9 +2,11 @@ import Position from "../interfaces/position";
 import Piece from "./piece.model";
 
 export default class Pawn extends Piece {
+    isFirstMove: boolean;
 
     constructor(position: Position) {
         super(position);
+        this.isFirstMove = true;
     }
 
     getMovements(canEatLeft?: boolean, canEatRight?: boolean): Array<Position> {
@@ -13,6 +15,11 @@ export default class Pawn extends Piece {
 
         newPosition.vertical += 1;
         movements.push(newPosition);
+        if(this.isFirstMove) {
+            newPosition.vertical += 1;
+            movements.push(newPosition);
+            newPosition.vertical -= 1;
+        }
         if(canEatRight) {
             newPosition.horizontal = this.position.horizontal + 1;
             movements.push(newPosition);
