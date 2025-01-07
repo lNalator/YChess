26c9800e041eb5191795b47bb973894bfaad9fce
Enzo Midonet <86829419+lNalator@users.noreply.github.com>
2025-01-07 16:17:05 +0100

    EM : second hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
 "use client";
+import { useState } from "react";
 import Timer from "@/components/Timer/Timer";
 import Grid from "@/components/Board/Grid";
 import MenuOverlay from "@/components/MenuOverlay/MenuOverlay";
-import "./page.css";
 import PiecesHelper from "@/core/helpers/pieces.helper";
 import { ColorEnum } from "@/core/enums/color.enum";
 import Piece from "@/core/entities/piece.model";
+import "./page.css";
 
 export default function Home() {
   const [time, setTime] = useState(0);
